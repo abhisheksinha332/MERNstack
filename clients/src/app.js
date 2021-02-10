f238eefccffe9784234773d1a89ffea2af6139c2
Abhishek Sinha <3327abhishek@gmail.com>
2021-02-11 00:12:48 +0530

    action declaration

--- clients/src/app.js
+++ clients/src/app.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, AppBar, Typography, Grow , Grid} from '@material-ui/core';
+import {useDispatch } from 'react-redux';
+
+import {getPosts} from './actions/Posts';
 
 import feeds from './images/pen.png';
 import Posts from './components/posts/Posts'
@@ -8,6 +11,12 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(getPosts());
+    },[dispatch]);
+
     return(
         <Container maxWidth="lg">
             <AppBar className={classes.appBar } position="static" color="inherit">
