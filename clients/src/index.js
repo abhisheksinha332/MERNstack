9aefa78f99c7d167fa608f244ec9e2246cc389ab
Abhishek Sinha <3327abhishek@gmail.com>
2021-02-19 22:18:16 +0530

    svg background

--- clients/src/index.js
+++ clients/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './app';
+import './index.css';
 
 //redux setup
 const store = createStore(reducers,compose(applyMiddleware(thunk)))
